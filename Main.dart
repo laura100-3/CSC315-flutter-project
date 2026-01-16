b7bcc4e591d95acc8f2162907510b8fceacb3443
laura <nwakpachimusonma@gmail.com>
2026-01-16 05:39:20 +0100

    Update Main.dart

--- Main.dart
+++ Main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/screen/login.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true, // Enable Material 3 for modern design
+      ),
+      debugShowCheckedModeBanner: false, // Hide debug banner
+      home: const Login(),
+    );
+  }
+}
